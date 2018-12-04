7c45fa0ddffbe36d56591ebda2507a8c1c5a7162
csi <crisantip@hotmail.com>
2018-12-03 21:07:36 -0500

    Agrega ejemplo de formateo básico de fechas y tiempos

---
+++ src/cap3/date/time/format/_1Format.java
@@ -0,0 +1,21 @@
+package cap3.date.time.format;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+
+public class _1Format {
+
+	
+	public static void main(String[] args) {
+		
+		LocalDate fecha = LocalDate.of(1982, Month.APRIL, 9);
+		LocalTime tiempo = LocalTime.of(10, 50);
+		LocalDateTime fechaTiempo = LocalDateTime.of(fecha, tiempo);
+		System.out.println(fecha.format(DateTimeFormatter.ISO_DATE));
+		System.out.println(tiempo.format(DateTimeFormatter.ISO_TIME));
+		System.out.println(fechaTiempo.format(DateTimeFormatter.ISO_DATE_TIME));
+	}
+}
