a96ac6d514e7c00e8c41c117a3bd64ab597b900b
Katty Tamayo <kata_alexa@hotmail.com>
2018-12-17 23:44:40 -0500

    Modificadores de acceso

---
+++ cap4/pond/shore/Bird.java
@@ -0,0 +1,10 @@
+package pond.shore;
+
+public class Bird {
+	protected String text = "floating"; // protected access
+
+	protected void floatInWater() { // protected access
+		System.out.println(text);
+	}
+
+}
---
+++ cap4/pond/swan/BadCygnet.java
@@ -0,0 +1,14 @@
+package pond.swan;
+
+import pond.duck.MotherDuck; // import another package
+
+public class BadCygnet {
+
+	public void makeNoise() {
+		MotherDuck duck = new MotherDuck();
+		/*
+		duck.quack(); // DOES NOT COMPILE
+		System.out.println(duck.noise); // DOES NOT COMPILE
+		*/
+	}
+}
